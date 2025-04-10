15df724e1ef6e527752869a908f315cf60155c3f
leocoder <2942894660@qq.com>
2025-04-10 11:44:16 +0800

    feat: 分类管理页面修复

--- src/api/index.ts
+++ src/api/index.ts
@@ -3,13 +3,17 @@
 // API 更新时间：
 // API 唯一标识：
 import * as fenleiguanli from "./fenleiguanli";
+import * as biaoqianguanli from "./biaoqianguanli";
 import * as yonghuguanli from "./yonghuguanli";
+import * as biaoqianguanxiguanli from "./biaoqianguanxiguanli";
 import * as fenleiguanxiguanli from "./fenleiguanxiguanli";
 import * as basicErrorController from "./basicErrorController";
 import * as dengluguanli from "./dengluguanli";
 export default {
   fenleiguanli,
+  biaoqianguanli,
   yonghuguanli,
+  biaoqianguanxiguanli,
   fenleiguanxiguanli,
   basicErrorController,
   dengluguanli,
